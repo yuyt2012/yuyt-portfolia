1bf5583783803028e1a5bf84c8515ac273028aab
yuyt2012 <yuyt2012@gmail.com>
2024-01-10 00:00:32 +0900

    도메인 모델과 테이블 설계 엔티티 클래스 작성

---
+++ src/main/java/jpabook/jpashopstudy/JpashopStudyApplication.java
@@ -0,0 +1,12 @@
+package jpabook.jpashopstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpashopStudyApplication.class, args);
+    }
+}
---
+++ src/test/java/jpabook/jpashopstudy/JpashopStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpashopstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
