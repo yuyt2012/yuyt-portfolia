9b119f3f368086892018b2001fc44f316e9071ef
yuyt2012 <yuyt2012@gmail.com>
2024-01-12 13:48:06 +0900

    css 적용 및 메인페이지 작성

---
+++ src/main/java/jpabook/jpashopstudy/controller/MemberController.java
@@ -0,0 +1,22 @@
+package jpabook.jpashopstudy.controller;
+
+import jpabook.jpashopstudy.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+}
